d25796b96cea2b0d78a36de67e9091cadd02a335
wandji20 <wbertrand10@gmail.com>
2022-01-03 21:11:35 +0100

    Add sneaker show view

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,9 +1,15 @@
 class SneakersController < ApplicationController
+  before_action :set_sneaker, only: :show
   def index
     @brands = Brand.all
     @genders = Gender.all
   end
 
   def show
+  end
+
+  private
+  def set_sneaker
+    @sneaker = Sneaker.find(params[:id])
   end
 end
