ae0c9c39911f40fd29d6912259d47a733e40ca12
wandji20 <wbertrand10@gmail.com>
2022-01-06 06:17:24 +0100

    Add search options and filters in sneakers index view and add scopes to sneaker model

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -3,7 +3,7 @@ class SneakersController < ApplicationController
   def index
     @brands = Brand.all
     @genders = Gender.all
-    @pagy, @sneakers = pagy(Sneaker.all)
+    @pagy, @sneakers = pagy(scoped_records)
     @sneakers.includes(:brand, :gender)
   end
 
@@ -14,5 +14,19 @@ class SneakersController < ApplicationController
   private
   def set_sneaker
     @sneaker = Sneaker.find(params[:id])
+  end
+
+  def scoped_records
+    records = Sneaker.all
+    if params[:brand]
+      records = records.joins(:brand).where(brand: {name: params[:brand]})
+    end
+    if params[:gender]
+      records = records.joins(:gender).where(gender: {name: params[:gender]})
+    end
+    if params['sort-by']
+      records = records.send(params['sort-by'])
+    end
+    records
   end
 end
--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,4 +1,10 @@
 class Sneaker < ApplicationRecord
   belongs_to :brand
   belongs_to :gender
+
+  scope :filter_by_brand, -> (brand_id) {where('brand_id = ?', brand_id)}
+  scope :filter_by_gender, -> (gender_id) {where('gender_id = ?', gender_id)}
+  scope :increasing_price, -> {order(price: :asc)}
+  scope :decreasing_price, -> {order(price: :desc)}
+  scope :release_date, -> {order(release_date: :desc)}
 end
