b042a0d26e5f1259167d350b8347aee6b940c729
wandji20 <wbertrand10@gmail.com>
2022-01-19 23:09:43 +0100

    Add seed data

---
+++ db/seeds/01_brands.rb
@@ -0,0 +1,16 @@
+brands = [
+  {"name":"converse"},
+  {"name":"jordan"},
+  {"name":"puma"},
+  {"name":"nike"},
+  {"name":"adidas"},
+  {"name":"reebok"},
+  {"name":"diadora"},
+  {"name":"new balance"},
+  {"name":"crocs"},
+  {"name":"vans"},
+  {"name":"saucony"},
+  {"name":"asics"}
+]
+
+Brand.create(brands)
---
+++ db/seeds/02_genders.rb
@@ -0,0 +1,10 @@
+genders = [
+  {"name":"men"},
+  {"name":"toddler"},
+  {"name":"preschool"},
+  {"name":"child"},
+  {"name":"women"},
+  {"name":"unisex"}
+]
+
+Gender.create(genders)
