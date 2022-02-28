04b8cd2f4365b931781f17861135bf6f694bfa1f
wandji20 <wbertrand10@gmail.com>
2022-02-28 22:09:20 +0100

    Add order_item factory and model specs

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_numericality_of(:quantity).is_greater_than_or_equal_to(1) }
+  it { should belong_to(:sneaker) }
 end
