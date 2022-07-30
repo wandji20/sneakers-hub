778d979aff117813e814b335d500ba28db28be38
wandji20 <wbertrand10@gmail.com>
2022-07-30 20:04:25 +0100

    Add post endpoint for stripe webhooks and order show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
-  resources :orders, only: [:create]
+  resources :orders, only: %i[create show]
   mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
@@ -9,12 +9,13 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :update]
   resources :order_items, except: [:new ]
   resources :sneakers, only: [:index, :show]
-  resources :sessions, only: [:create]
   resources :brands, only: :index
   resources :genders, only: :index
-  root to: 'pages#home'
   get 'about', to: "pages#about"
   post 'checkout', to: "pages#checkout"
   get 'shipping', to: 'pages#shipping'
+  post 'stripe-webhook', to: 'orders#stripe_webhook'
+  
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
